5413e318656e36ef51912c1ebe1c72c25a33140c
JValbjorn <julievalbjorn@live.dk>
2025-04-02 11:10:50 +0200

    move left

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "")
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 1}px`;
+    }
+    console.log(event);
+});
+
+
+
