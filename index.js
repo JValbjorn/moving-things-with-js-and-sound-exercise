1d96aea0de10269fa36cd1313d1a6893923bf530
JValbjorn <julievalbjorn@live.dk>
2025-04-02 11:48:20 +0200

    move right

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 // Your code here
+"use strict";
 
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  }
-
-  document.addEventListener("keydown", function (e) {
+document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
+      
     }
-  });
+    });
+    
+     
 
+    function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+  
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
+         }}
 
-
+         function moveDodgerRight() {
+            const leftNumbers = dodger.style.left.replace("px", "");
+            const left = parseInt(leftNumbers, 10);
+      
+            if (left < 360) {
+                dodger.style.left = `${left + 1}px`;
+             }}
+             
+             document.addEventListener("keydown", function (e) {
+                 if (e.key === "ArrowLeft") {
+                     moveDodgerLeft();
+                 } else if (e.key === "ArrowRight") {
+                     moveDodgerRight();
+                 } else if (e.key === "ArrowUp") { // Calls moveDodgerUp() when Up Arrow is pressed
+                     moveDodgerUp();
+                 } else if (e.key === "ArrowDown") { // Calls moveDodgerDown() when Down Arrow is pressed
+                     moveDodgerDown();
+                 }
+             });
