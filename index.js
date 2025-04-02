c80f086293bdee353f810b2c3defcfd86ff5279c
JValbjorn <julievalbjorn@live.dk>
2025-04-02 11:13:36 +0200

    cant move out of the box

--- index.js
+++ index.js
@@ -3,15 +3,20 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
-
-document.addEventListener("keydown", function(event) {
-    if (event.key === "ArrowLeft") {
-        const leftNumbers = dodger.style.left.replace("px", "")
-        const left = parseInt(leftNumbers, 10);
-        dodger.style.left = `${left - 1}px`;
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
     }
-    console.log(event);
-});
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
 
 
 
